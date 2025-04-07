1fe3205aa60f8a0a814b0940537890e768aeaac9
Eduard Bukin <edwardbukin@gmail.com>
2025-04-07 18:21:15 -0400

    Clarifying the code for SILC data calculations

--- 10-SILC-variables.R
+++ 10-SILC-variables.R
@@ -10,6 +10,7 @@ library(haven)
 library(dplyr)
 library(purrr)
 library(tidyr)
+library(stringr)
 library(readr)
 library(Hmisc)
 library(openxlsx)
@@ -58,43 +59,51 @@ df_hh <-
   df |> 
   group_by(code, year, hhid) |> 
   summarise(
-    across(c(hx090, hx040, hx050, rb050, rx070, psu, strata, subcode), ~ first(.))
+    across(c(hx090, hx040, hx050, rb050, rx070, psu, strata, subcode),
+           ~ first(.)), 
+    .groups = "drop"
   ) |> 
-  ungroup() |> 
     
   # We must group by country/year in order to compute all statistics on the annual basis.
   group_by(code, year) |> 
   mutate(
     popw = rb050 * hx040,                           # population weights 
-    
     inc_pc_euro = (hx090 * hx050) / hx040,          # HH disposable income per capita
-    
     inc_adeq_euro = hx090,                          # HH disposable income per adult equivalent
     
-    inc_adeq_euro_40 = 
-      ifelse(inc_adeq_euro <= Hmisc::wtd.quantile(inc_adeq_euro, popw, .4, na.rm = TRUE), 
-             inc_adeq_euro, 
-             NA_real_),
+    arop = 0.6 * Hmisc::wtd.quantile(
+      x = inc_adeq_euro, 
+      weights = popw, 
+      probs = 0.5, 
+      na.rm = TRUE),
     
-    inc_adeq_euro_20 = 
-      ifelse(inc_adeq_euro <= Hmisc::wtd.quantile(inc_adeq_euro, popw, .2, na.rm = TRUE), 
-             inc_adeq_euro, 
-             NA_real_),
-    
-    # inc_adeq_euro_median = Hmisc::wtd.quantile(inc_adeq_euro, popw, probs = 0.5, na.rm = TRUE),
-    # # inc_adeq_euro_mean = Hmisc::wtd.mean(inc_adeq_euro, popw, na.rm = TRUE),
-    
-    arop = 0.6 * Hmisc::wtd.quantile(inc_adeq_euro, popw, probs = 0.5, na.rm = TRUE),
-    # arope = ifelse(is.na(rx070), NA, as.numeric(rx070 != 0)),
+    arope = ifelse(is.na(rx070), NA, as.numeric(rx070 != 0)),
     
     fgt0_arop = as.integer(inc_adeq_euro < arop),
     fgt1_arop = ifelse(fgt0_arop, 1 - (inc_adeq_euro / arop), 0),
     fgt2_arop = ifelse(fgt0_arop, (1 - (inc_adeq_euro / arop)) ^ 2, 0)
-    
   ) |> 
   select(-rb050, -hx040, -hx090, -hx050) |> 
-  ungroup() |> 
-  glimpse()
+  group_by(code, year, subcode) |> 
+  mutate(
+    b40_inc_adeq_euro =
+      ifelse(
+        inc_adeq_euro <= Hmisc::wtd.quantile(x = inc_adeq_euro, weights = popw,
+                                             probs = 0.4, na.rm = TRUE),
+        inc_adeq_euro,
+        NA_real_
+      ),
+
+    b20_inc_adeq_euro =
+      ifelse(
+        inc_adeq_euro <= Hmisc::wtd.quantile(x = inc_adeq_euro, weights = popw,
+                                             probs = 0.2, na.rm = TRUE),
+        inc_adeq_euro,
+        NA_real_
+      ),
+
+  ) |> 
+  ungroup() 
 
 # Survey-base aggregates that include design -----------------
 
@@ -106,92 +115,59 @@ survey_design <- svydesign(
   data = df_hh
 )
 
-stats_cols <- c("inc_adeq_euro", "inc_pc_euro", "fgt0_arop", "fgt1_arop", "fgt2_arop", 
-                "inc_adeq_euro_40", "inc_adeq_euro_20")
+# Variables list for which we compute means and variances ------------------
+# Note: Make sure check and report the number of missing observations in these variables
+stats_cols <- c("inc_adeq_euro", 
+                "fgt0_arop", "fgt1_arop", "fgt2_arop", "arope", 
+                "b40_inc_adeq_euro", "b20_inc_adeq_euro")
 
 # Regular population weights-weighted statistics
 df_wtd <- 
   df_hh |> 
   group_by(code, year, subcode) |> 
   summarise(
-    gini_adeq = reldist::gini(inc_adeq_euro, weights = popw),
+    n_obs = n(), # Number of observations per region
+    sum_popw = sum(popw),  # Sum of the population weights
+    gini_inc_adeq_euro = reldist::gini(x = inc_adeq_euro, weights = popw),
     across(any_of(stats_cols), 
-           ~ Hmisc::wtd.mean(., popw, na.rm = TRUE), 
-           .names = "wtd.mean_{.col}"),
-    
-    across(any_of(stats_cols), 
-           ~ Hmisc::wtd.quantile(., popw, probs = 0.5, na.rm = TRUE), 
-           .names = "wtd.median_{.col}"),
-    
-    across(any_of(stats_cols),
-           ~ Hmisc::wtd.var(., popw, na.rm = TRUE), 
-           .names = "wtd.var_{.col}")
-  ) |> 
-  ungroup() |> 
-  glimpse()
+           ~ Hmisc::wtd.mean(x = ., weights = popw, na.rm = TRUE), 
+           .names = "mean_{.col}"),
+    across(c(inc_adeq_euro), 
+           ~ Hmisc::wtd.quantile(x = ., weights = popw, probs = 0.5, na.rm = TRUE), 
+           .names = "median_{.col}"), 
+    .groups = "drop" 
+  )
 
-# Survey-design corrected means
-df_svywtd.mean <-
-  svyby(
-    formula = paste("~", paste(stats_cols, collapse = "+")) |> as.formula(),
-    by = ~ code + year + subcode,
-    design = survey_design,
-    FUN = svymean,
-    deff = TRUE,
-    na.rm = TRUE
-  ) |>
-  as_tibble() |> 
-  rename_with(~ paste0("svywtd.mean_", .), .cols = contains(stats_cols)) |> 
-  glimpse()
-
-# Survey-design corrected medians
-df_svywtd.median <-
-  svyby(
-    formula = paste("~", paste(stats_cols, collapse = "+")) |> as.formula(),
-    by = ~ code + year + subcode,
-    design = survey_design,
-    FUN = svyquantile,
-    deff = TRUE,
-    quantiles = 0.5,
-    na.rm = TRUE
-  )  |>
-  as_tibble() |> 
-  rename_with(~ paste0("svywtd.median_", .), .cols = contains(stats_cols)) |> 
-  glimpse()
-
-# Survey-design corrected variance
-df_svywtd.var <-
-  stats_cols |> 
-  map(~{
-    one_var <- .x
-    new_se_var <- paste0("se.", one_var)
+# Survey-design corrected variances
+df_svywtd <-
+  stats_cols |>
+  map( ~ {
+    var_focus <- .x
     svyby(
-      formula = paste("~", .x) |> as.formula(),
+      formula = paste("~", var_focus) |> as.formula(),
       by = ~ code + year + subcode,
       design = survey_design,
-      FUN = svyvar,
-      deff = TRUE,
-      na.rm = TRUE
-    )  |>
-      as_tibble() |> 
-      rename_with(~ new_se_var, .cols = contains("se")) |> 
-      rename_with(~ paste0("svywtd.var_", .), .cols = contains(stats_cols)) 
-  }) |> 
-  reduce(left_join, join_by(code, year, subcode))
+      FUN = svymean,
+      deff = FALSE,
+      na.rm = TRUE,
+      vartype = "var"
+    ) |>
+      as_tibble() |>
+      select(-any_of(var_focus)) |>
+      rename_with( ~ str_c("var_", var_focus), .cols = "var")
+  }) |>
+  reduce(left_join, by = join_by(code, year, subcode)) |>
+  arrange(year, subcode) 
 
 # Compiling all data ------------------------------------------------------
-df_out <- 
-  df_wtd |> 
-  left_join(df_svywtd.mean, by = join_by(code, year, subcode))|> 
-  left_join(df_svywtd.median, by = join_by(code, year, subcode))|> 
-  left_join(df_svywtd.var, by = join_by(code, year, subcode)) |> 
-  glimpse()
+df_out <- df_wtd |> left_join(df_svywtd, by = join_by(code, year, subcode))
 
 # Saving data -----------------------------------------------------------
 write_rds(df_out, file.path(data_aux, "eu-silc.rds"), compress = "gz")
 write_csv(df_out, file.path(data_aux, "eu-silc.csv"))
 
-# Save as Excel (.xlsx)
-write.xlsx(df_out, file.path(data_aux, "eu-silc.xlsx"),
-           sheetName = "EU-SILC Indicators",
-           overwrite = TRUE)
+# Save as Excel (.xlsx), if needed
+write.xlsx(df_out,
+           file.path(data_aux, "EU_SILC_Indicators.xlsx"),
+           sheetName = "R",
+           overwrite = F)
